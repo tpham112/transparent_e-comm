3433e11eef92844e933c1996ff2614edbd3018f2
bigmartha1 <tpham112@gmail.com>
2023-03-30 03:35:53 -0700

    Updated client testimonies section

--- js/script.js
+++ js/script.js
@@ -39,18 +39,3 @@ for (let i = 0; i < navLinks.length; i++) {
         body.style.overflow = "auto";
     })
 }
-
-// Swiper JS Code //
-var swiper = new Swiper(".mySwiper", {
-    slidesPerView: 1,
-    spaceBetween: 30,
-    loop: true,
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
-    },
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
-    },
-  });
