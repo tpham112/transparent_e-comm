70b43f56eb409147c30fb7bcf218ccdae4cf0539
bigmartha1 <tpham112@gmail.com>
2023-04-05 05:29:32 -0700

    Updated client logos to autoswiper

--- js/script.js
+++ js/script.js
@@ -39,3 +39,36 @@ for (let i = 0; i < navLinks.length; i++) {
         body.style.overflow = "auto";
     })
 }
+
+// Swiper JS Code //
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 1,
+    spaceBetween: 30,
+    loop: true,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+});
+
+var swiper = new Swiper(".autoswipe", {
+    slidesPerView: 1,
+    spaceBetween: 30,
+    loop: true,
+    autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+    },
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+});
